f34b07b25b047b9acd12c531fb8ce02439c13e83
ZhihaoZeng <1126286295@qq.com>
2020-01-03 11:02:58 +0800

    add cookie

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -30,28 +31,34 @@ public class AuthorizeController {
     private UserMapper userMapper;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
+                           HttpServletResponse response) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
+        //由client_id和client_secret从github获得一个access_token.
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        //再由github给的access_token从github获得用户的用户名、id、签名.
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null) {
+            //在本地数据库创建一个用户
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            //登录成功，写cookie和session
-            request.getSession().setAttribute("user", githubUser);
-        } else {
-            //登录失败，重新登录
+            response.addCookie(new Cookie("token", token));
+            return "redirect:/";
         }
-        return "redirect:/";
+        else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
